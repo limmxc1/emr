388a70a934089fc4a58ded1511bf03071ded98b5
Cheval Lim <limmxc1@gmail.com>
2023-10-31 22:41:44 +0800

    connected TailwindCSS to React

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
